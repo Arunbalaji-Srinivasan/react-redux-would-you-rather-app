a2a78812934f2876807b4ace1581340a0c79c1d3
Arunbalaji Srinivasan <59905487+Arunbalaji-Srinivasan@users.noreply.github.com>
2020-06-07 19:30:02 -0400

    Update _DATA.js

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
     sarahedo: {
       id: 'sarahedo',
       name: 'Sarah Edo',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/sarah.jpg',
+      avatarURL: 'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png',
       answers: {
         "8xf0y6ziyjabvozdd253nd": 'optionOne',
         "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
     tylermcginnis: {
       id: 'tylermcginnis',
       name: 'Tyler McGinnis',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/tyler.jpg',
+      avatarURL: 'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png',
       answers: {
         "vthrdm985a262al8qx3do": 'optionOne',
         "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
     johndoe: {
       id: 'johndoe',
       name: 'John Doe',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/dan.jpg',
+      avatarURL: 'https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png',
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
         "vthrdm985a262al8qx3do": 'optionTwo',
